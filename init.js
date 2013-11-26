4049a1c59afd6dc8506df8967a1ec317c9cc0d8c
Scott Noel-Hemming <frogstarr78@gmail.com>
2013-11-25 23:57:43 -0800

    Adding start of project.

---
+++ init.js
@@ -0,0 +1,55 @@
+var http    = require('http'),
+    url     = require('url'),
+	fs      = require('fs'),
+	path    = require('path'),
+	util    = require('util'),
+	log     = console.log,
+	debug   = util.debug,
+	inspect = console.dir,
+	cfVm    = require('./cfvm');
+
+
+var serveFile = function(rPath, response) {
+	fs.exists(rPath, function (exs) {
+		if ( exs ) {
+			response.writeHead(200, {'Content-Type': 'text/html'});
+			fs.readFile(rPath, function(err, data) {
+				if ( err ) throw err;
+				response.end(data);
+			});
+		} else {
+			response.writeHead(404, {'Content-Type': 'text/plain'});
+			response.end("Requested file '" + rPath + "' doesn't exist\n");
+		}
+	});
+}
+
+var handleResponse = function(request, response) {
+	var reqPath = path.join(__dirname, url.parse(request.url).pathname)
+
+	fs.exists(reqPath, function (ex) {
+		if( ex ) {
+			fs.stat(reqPath, function (err, stat) {
+				if ( err ) throw err;
+
+				if ( stat.isFile() ) {
+					serveFile(reqPath, response);
+				} else if ( stat.isDirectory() ) {
+					reqPath = path.join(reqPath, 'index.cfm');
+					serveFile(reqPath, response);
+				}
+			});
+		} else {
+			response.writeHead(404, {'Content-Type': 'text/plain'});
+			response.end("Requested file '" + reqPath + "' doesn't exist\n");
+		}
+	});
+}
+
+http.createServer(function (request, response) {
+	var docRoot = __dirname;
+	handleResponse(request, response);
+
+}).listen(8124, 'localhost');
+
+log('Server running at http://127.0.0.1:8124/');
