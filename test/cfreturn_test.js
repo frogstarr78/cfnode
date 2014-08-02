90db0e47f505fb0d78b26c194136d6bf6fea87f8
Scott Noel-Hemming <frogstarr78@gmail.com>
2014-08-02 02:29:36 -0700

    Adding tests for cfreturn tag

---
+++ test/cfreturn_test.js
@@ -0,0 +1,31 @@
+var is = require('assert'),
+	util = require('util'),
+	path = require('path'),
+//	human_date = require('date.js'),
+	PEG = require('pegjs'),
+	cf = require(__dirname + '/../cf'),
+	testlib = require('./testlib');
+
+var r;
+
+is.throws(function () {
+	r = cf.parse('<cfreturn>');
+}, Error, 'Missing required expression.');
+
+is.throws(function () {
+	r = cf.parse('<cfreturn >');
+}, Error, 'Missing required expression.');
+
+r = cf.parse('<cfreturn TRIM(username)>');
+is.equal(r instanceof Object, true);
+is.equal(r.tag, 'return');
+is.equal(r.expression, 'TRIM(username)');
+is.deepEqual(r.attributes, {});
+
+r = cf.parse('<CFRETURN 1 NE 0>');
+is.equal(r instanceof Object, true);
+is.equal(r.tag, 'return');
+is.equal(r.expression, '1 NE 0');
+is.deepEqual(r.attributes, {});
+
+testlib.die("Success!", 0);
