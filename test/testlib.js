d19f9f8843b5f7d9a7c68106c67621370fc0e58f
Scott Noel-Hemming <frogstarr78@gmail.com>
2014-08-01 21:23:56 -0700

    Adding better color

--- test/testlib.js
+++ test/testlib.js
@@ -1,10 +1,20 @@
+var util = require('util'),
+	colors = require('colors');
+
 function inspect(o) {
-	console.log(require('util').inspect(o, { colors: true, depth: null}));
+	console.log(util.inspect(o, { colors: true, depth: null}));
 }
 exports.inspect = inspect;
 
 function die(content, exitStatus) { 
-	inspect(content||'Aborted!');
+	var xs = typeof exitStatus === 'undefined' ? 1 : exitStatus;
+	if( typeof content === 'undefined' ) {
+		console.log('Aborted!'.red)
+	} else if ( typeof content === 'string' && content != 'Success!' ) {
+		console.log(content)
+	} else {
+		inspect(content);
+	}
 	process.exit(exitStatus||1);
 }
 
