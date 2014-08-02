03c7a0a05f0247e0f3f1a1bfd27f2e080f4ee99d
Scott Noel-Hemming <frogstarr78@gmail.com>
2014-08-02 02:29:08 -0700

    Fixing wrong test

--- test/cfelse_test.js
+++ test/cfelse_test.js
@@ -8,14 +8,32 @@ var is = require('assert'),
 
 var r;
 
-r = cf.parse('<cfthrow>');
+is.throws(function () {
+	r = cf.parse('<cfelse>');
+}, Error, 'Missing required closing tag');
+
+r = cf.parse('<cfelse></cfif>');
 is.equal(r instanceof Object, true);
-is.equal(r.tag, 'throw');
+is.equal(r.tag, 'else');
 is.equal(r.content, '');
 
-r = cf.parse('<CFTHROW>');
+r = cf.parse('<CFELSE></cfif>');
 is.equal(r instanceof Object, true);
-is.equal(r.tag, 'throw');
+is.equal(r.tag, 'else');
 is.equal(r.content, '');
+
+r = cf.parse('<cfelse>' + 
+"\nOtherwise we do this" +
+'</cfif>');
+is.equal(r instanceof Object, true);
+is.equal(r.tag, 'else');
+is.equal(r.content, "\nOtherwise we do this");
+
+r = cf.parse('<CFELSE>' + 
+"\nOtherwise we do this.\nStrang looking test." +
+'</CFIF>');
+is.equal(r instanceof Object, true);
+is.equal(r.tag, 'else');
+is.equal(r.content, "\nOtherwise we do this.\nStrang looking test.");
 
 testlib.die("Success!", 0);
