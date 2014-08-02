24866c8acdd4f41c6fb534eba2252a563fb2ea78
Scott Noel-Hemming <frogstarr78@gmail.com>
2014-08-01 21:25:03 -0700

    Fixing most of the tests that I was previously bypassing

--- test/cfhtmlhead_test.js
+++ test/cfhtmlhead_test.js
@@ -13,20 +13,23 @@ is.throws(function () {
 	r = cf.parse('<cfhtmlhead>');
 }, Error, "Missing required text attribute");
 
-r = cf.parse('<cfhtmlhead text="cftmlhead test">');
-testlib.die();
+r = cf.parse('<cfhtmlhead text="cfhtmlhead test">');
 is.equal(r instanceof Object, true);
 is.equal(r.tag, 'htmlhead');
 is.equal(r.attributes.text, 'cfhtmlhead test');
 
-r = cf.parse('<cfhtmlhead text="<link href=\"/blog/custom/img/favicon.ico\" rel=\"shortcut icon\" type=\"image/x-icon\">#chr(13)##chr(10)#" />');
+r = cf.parse('<cfhtmlhead text="#chr(13)##chr(10)#" />');
 is.equal(r instanceof Object, true);
 is.equal(r.tag, 'htmlhead');
-is.equal(r.attributes.text, "<link href=\"/blog/custom/img/favicon.ico\" rel=\"shortcut icon\" type=\"image/x-icon\">#chr(13)##chr(10)#");
+is.equal(r.attributes.text, "#chr(13)##chr(10)#");
 
 r = cf.parse('<CFHTMLHEAD TEXT="cfhtmlhead test">');
 is.equal(r instanceof Object, true);
 is.equal(r.tag, 'htmlhead');
 is.equal(r.attributes.text, 'cfhtmlhead test');
+
+//@TODO Fix this test
+testlib.die("Fix Me!".red, 1);
+//r = cf.parse('<cfhtmlhead text="<link href='"'/blog/custom/img/favicon.ico\' rel=\'shortcut icon\' type=\'image/x-icon\'>#chr(13)##chr(10)#" />');
 
 testlib.die("Success!", 0);
