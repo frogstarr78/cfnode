0cabeeca8097a9ca110628574b34bd7dd5cae110
Scott Noel-Hemming <frogstarr78@gmail.com>
2014-09-07 22:28:12 -0700

    Simplifying

--- test/testlib.js
+++ test/testlib.js
@@ -1,12 +1,26 @@
-var util = require('util'),
+var path = require('path'),
+	fs   = require('fs'),
+	join = path.join,
+	util = require('util'),
 	colors = require('colors');
 
-function inspect(o) {
-	console.log(util.inspect(o, { colors: true, depth: null}));
-}
-exports.inspect = inspect;
+exports.exit = process.exit;
+exports.fail = function () { console.log('X'.red); };
+exports.neut = function () { console.log('-'.yellow); };
+exports.ok = function () { console.log('√'.green); };
+exports.inspect = function (s) {
+	console.log(util.inspect(s, { depth: null} ));
+};
 
-function die(content, exitStatus) { 
+exports.log = util.log;
+exports.dir = util.inspect;
+exports.error = function (m, nl) { 
+	var nl = nl || false;
+	var fmt = nl ? "\n - %s " : " - %s ";
+	util.print(util.format(fmt, m)); 
+};
+
+exports.die = function (content, exitStatus) { 
 	var xs = typeof exitStatus === 'undefined' ? 1 : exitStatus;
 	if( typeof content === 'undefined' ) {
 		console.log('Aborted!'.red)
@@ -18,5 +32,13 @@ function die(content, exitStatus) {
 	process.exit(exitStatus||1);
 }
 
-exports.die = die;
-
+function root() { 
+	var r = path.resolve(__dirname, '..'),
+		sp = '';
+	for(var i=0; i < arguments.length; i++) {
+		sp += path.join( '/', arguments[i] );
+	}
+	return path.join(r,  sp);
+} 
+exports.root = root;
+//exports.you = function () { return require(join(root(), 'you')); };
