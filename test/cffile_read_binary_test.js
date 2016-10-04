9ecdd0581c7d17d5ed8579998746e180bca44a01
Scott Noel-Hemming <frogstarr78@gmail.com>
2016-10-04 15:20:30 -0700

    While using this as a template for making the cfzip action=readBinary test, I found some bugs and fixed them

--- test/cffile_read_binary_test.js
+++ test/cffile_read_binary_test.js
@@ -17,27 +17,38 @@ is.throws(function () {
 }, Error, 'Missing required action attribute.');
 
 is.throws(function () {
-	r = cf.parse('<cffile action="read" file="/tmp/file" >');
+	r = cf.parse('<cffile action="readbinary" file="/tmp/file" >');
 }, Error, 'Missing required variable attribute.');
 
 is.throws(function () {
-	r = cf.parse('<cffile action="read" variable="something" />');
+	r = cf.parse('<cffile action="readbinary" variable="something" />');
 }, Error, 'Missing required file attribute.');
 
-r = cf.parse('<cffile action="read" variable="cffile_test" file="/tmp/file" >');
+r = cf.parse('<cffile action="readbinary" variable="cffile_test" file="/tmp/file" >');
 is.equal(r instanceof Object, true);
 is.equal(r.tag, 'file');
-is.equal(r.attributes.action, 'read');
+is.equal(r.attributes.action, 'readbinary');
 is.equal(r.attributes.variable, 'cffile_test');
 is.equal(r.attributes.file, '/tmp/file');
 
 r = cf.parse('<CFFILE ' +
-'ACTION="read" ' +
+'ACTION="read_binary" ' +
 'VARIABLE="cffile_test3" ' +
 'FILE="/tmp/file" />');
 is.equal(r instanceof Object, true);
 is.equal(r.tag, 'file');
-is.equal(r.attributes.action, 'read');
+is.equal(r.attributes.action, 'read_binary');
+is.equal(r.attributes.variable, 'cffile_test3');
+is.equal(r.attributes.file, '/tmp/file');
+
+
+r = cf.parse('<CFFILE ' +
+'ACTION="readBinary" ' +
+'VARIABLE="cffile_test3" ' +
+'FILE="/tmp/file" />');
+is.equal(r instanceof Object, true);
+is.equal(r.tag, 'file');
+is.equal(r.attributes.action, 'read_binary');
 is.equal(r.attributes.variable, 'cffile_test3');
 is.equal(r.attributes.file, '/tmp/file');
 
