229d3cb599f5cb616350942b64a5382a830ac357
Scott Noel-Hemming <frogstarr78@gmail.com>
2018-03-12 16:07:06 -0700

    Moved to use mocha testing framework

--- test/cfimport_test.js
+++ test/cfimport_test.js
@@ -1,39 +1,49 @@
-const is = require('assert'), test = require('./testlib');
+const test = require('./testlib'), should = require('should');
 
 var r;
-is.throws(function () {
-	r = test.cfparser.parse('<cfimport>');
-}, Error, "Missing required attributes");
 
-is.throws(function () {
-	r = test.cfparser.parse('<cfimport taglib="/path/to/taglib.cfc">');
-}, Error, "Missing required prefix attribute");
+describe('parsing a cfimport tag', function () {
+    it('throws an error when missing required attributes', function () {
+        (function () { test.cfparser.parse('<cfimport>') }).should.throw('Missing required "taglib" attribute.');
+    });
 
-is.throws(function () {
-	r = test.cfparser.parse('<cfimport prefix="cfnode_test">');
-}, Error, "Missing required taglib attribute");
+    it('throws an error when missing the required "prefix" attribute', function () {
+        (function () { test.cfparser.parse('<cfimport taglib="/path/to/taglib.cfc">') }).should.throw('Missing required "prefix" attribute.');
+    });
 
-r = test.cfparser.parse('<cfimport taglib="/path/to/taglib.jsp" prefix="">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'import');
-is.equal(r.attributes.prefix, '');
-is.equal(r.attributes.taglib, '/path/to/taglib.jsp');
+    it('throws an error when missing the required "taglib" attribute', function () {
+        (function () { test.cfparser.parse('<cfimport prefix="cfnode_test">') }).should.throw('Missing required "taglib" attribute.');
+    });
 
-r = test.cfparser.parse('<cfimport taglib="/path/to/taglib.jsp" prefix="cfnode" />');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'import');
-is.equal(r.attributes.prefix, 'cfnode');
-is.equal(r.attributes.taglib, '/path/to/taglib.jsp');
+    it('should work as expected', function () {
+        r = test.cfparser.parse('<cfimport taglib="/path/to/taglib.jsp" prefix="">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('import');
+        r.attributes.prefix.should.eql('');
+        r.attributes.taglib.should.eql('/path/to/taglib.jsp');
+    });
 
-r = test.cfparser.parse('<cfimport prefix="" taglib="/path/to/taglib.cfc">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'import');
-is.equal(r.attributes.prefix, '');
-is.equal(r.attributes.taglib, '/path/to/taglib.cfc');
+    it('should work as expected with different attribute values', function () {
+        r = test.cfparser.parse('<cfimport taglib="/path/to/taglib.jsp" prefix="cfnode" />');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('import');
+        r.attributes.prefix.should.eql('cfnode');
+        r.attributes.taglib.should.eql('/path/to/taglib.jsp');
+    });
 
-r = test.cfparser.parse('<CFIMPORT PREFIX="" TAGLIB="/path/to/taglib.cfc">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'import');
-is.equal(r.attributes.prefix, '');
-is.equal(r.attributes.taglib, '/path/to/taglib.cfc');
+    it('should work as expected with empty prefix attribute', function () {
+        r = test.cfparser.parse('<cfimport prefix="" taglib="/path/to/taglib.cfc">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('import');
+        r.attributes.prefix.should.eql('');
+        r.attributes.taglib.should.eql('/path/to/taglib.cfc');
+    });
 
+    it('should work as expected with all-caps attributes', function () {
+        r = test.cfparser.parse('<CFIMPORT PREFIX="" TAGLIB="/path/to/taglib.cfc">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('import');
+        r.attributes.prefix.should.eql('');
+        r.attributes.taglib.should.eql('/path/to/taglib.cfc');
+    });
+});
