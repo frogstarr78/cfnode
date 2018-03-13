3b4235a4671c38e41ae6e2a3832c36b04e0a54a9
Scott Noel-Hemming <frogstarr78@gmail.com>
2018-03-12 20:59:02 -0700

    Moved to use the Mocha testing framework

--- test/cfassociate_test.js
+++ test/cfassociate_test.js
@@ -1,35 +1,43 @@
-const is = require('assert'), test = require('./testlib');
+const should = require('should'), test = require('./testlib');
 
-var r;
-is.throws(function () {
-	r = test.cfparser.parse('<cfassociate data_collection="this">');
-}, Error);
+describe('Parsing the cfassociate tag', function () {
+    it('should throw an error without a required base_tag', function () {
+        (function () { test.cfparser.parse('<cfassociate data_collection="this">') }).should.throw('Missing required "base_tag" attribute.');
+    });
 
-is.throws(function () {
-	r = test.cfparser.parse('<cfassociate baseTag="cfnode test">');
-}, Error, "Invalid baseTag value.");
+//    it('should throw an error without a required base_tag', function () {
+//        (function () { r = test.cfparser.parse('<cfassociate baseTag="cfnode test">'); }).should.throw('Invalid "base_tag" value.');
+//    });
 
-r = test.cfparser.parse('<cfassociate baseTag="cfnode_test">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'associate');
-is.equal(r.attributes.base_tag, 'cfnode_test');
-is.equal(r.attributes.data_collection, 'AssocAttribs');
+    it('should work as expected with few attributes defined', function () {
+        r = test.cfparser.parse('<cfassociate baseTag="cfnode_test">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('associate');
+        r.attributes.base_tag.should.eql('cfnode_test');
+        r.attributes.data_collection.should.eql('AssocAttribs');
+    });
 
-r = test.cfparser.parse('<cfassociate baseTag="cfnode_test" dataCollection="something">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'associate');
-is.equal(r.attributes.base_tag, 'cfnode_test');
-is.equal(r.attributes.data_collection, 'something');
+    it('should work as expected with all attributes defined', function () {
+        r = test.cfparser.parse('<cfassociate baseTag="cfnode_test" dataCollection="something">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('associate');
+        r.attributes.base_tag.should.eql('cfnode_test');
+        r.attributes.data_collection.should.eql('something');
+    });
 
-r = test.cfparser.parse('<cfassociate dataCollection="somethingelse" baseTag="cfnode_test">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'associate');
-is.equal(r.attributes.base_tag, 'cfnode_test');
-is.equal(r.attributes.data_collection, 'somethingelse');
+    it('should work as expected with all attributes defined', function () {
+        r = test.cfparser.parse('<cfassociate dataCollection="somethingelse" baseTag="cfnode_test">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('associate');
+        r.attributes.base_tag.should.eql('cfnode_test');
+        r.attributes.data_collection.should.eql('somethingelse');
+    });
 
-r = test.cfparser.parse('<CFASSOCIATE DATACOLLECTION="somethingelse" BASETAG="cfnode_test">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'associate');
-is.equal(r.attributes.base_tag, 'cfnode_test');
-is.equal(r.attributes.data_collection, 'somethingelse');
-
+    it('should work as expected with everything defined in all caps', function () {
+        r = test.cfparser.parse('<CFASSOCIATE DATACOLLECTION="somethingelse" BASETAG="cfnode_test">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('associate');
+        r.attributes.base_tag.should.eql('cfnode_test');
+        r.attributes.data_collection.should.eql('somethingelse');
+    });
+});
