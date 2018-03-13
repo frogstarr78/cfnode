57fe1a7f8ef6735b7e048d61fdea32c9b5235464
Scott Noel-Hemming <frogstarr78@gmail.com>
2018-03-12 22:29:36 -0700

    Changed to use the Mocha testing framework

--- test/cfcontent_test.js
+++ test/cfcontent_test.js
@@ -1,35 +1,41 @@
-const is = require('assert'), test = require('./testlib');
+const should = require('should'), test = require('./testlib');
 
-var r;
+describe('Parsing the cfcontent tag', function () {
+    it('should behave as expected with no attributes defined', function () {
+        r = test.cfparser.parse('<cfcontent>');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('content');
+        r.attributes.delete_file.should.eql(false);
+        r.attributes.reset.should.eql(true);
+    });
 
+    it('should behave as expected with an attribute defined', function () {
+        r = test.cfparser.parse('<cfcontent deleteFile="yes">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('content');
+        r.attributes.delete_file.should.eql(true);
+        r.attributes.reset.should.eql(true);
+    });
 
-r = test.cfparser.parse('<cfcontent>');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'content');
-is.equal(r.attributes.delete_file, false);
-is.equal(r.attributes.reset, true);
+    it('should behave as expected with a bunch of attributes defined', function () {
+        r = test.cfparser.parse('<cfcontent deleteFile="yes" file="/path/to/file" reset="no" type="utf-8" variable="cfcontent_var" />');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('content');
+        r.attributes.delete_file.should.eql(true);
+        r.attributes.file.should.eql('/path/to/file');
+        r.attributes.reset.should.eql(false);
+        r.attributes.type.should.eql('utf-8');
+        r.attributes.variable.should.eql('cfcontent_var');
+    });
 
-r = test.cfparser.parse('<cfcontent deleteFile="yes">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'content');
-is.equal(r.attributes.delete_file, true);
-is.equal(r.attributes.reset, true);
-
-r = test.cfparser.parse('<cfcontent deleteFile="yes" file="/path/to/file" reset="no" type="utf-8" variable="cfcontent_var" />');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'content');
-is.equal(r.attributes.delete_file, true);
-is.equal(r.attributes.file, '/path/to/file');
-is.equal(r.attributes.reset, false);
-is.equal(r.attributes.type, 'utf-8');
-is.equal(r.attributes.variable, 'cfcontent_var');
-
-r = test.cfparser.parse('<CFCONTENT DELETEFILE="yes" FILE="/path/to/file" RESET="no" TYPE="utf-8" VARIABLE="cfcontent_var2" />');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'content');
-is.equal(r.attributes.delete_file, true);
-is.equal(r.attributes.file, '/path/to/file');
-is.equal(r.attributes.reset, false);
-is.equal(r.attributes.type, 'utf-8');
-is.equal(r.attributes.variable, 'cfcontent_var2');
-
+    it('should behave as expected with a bunch of attributes defined all in caps', function () {
+        r = test.cfparser.parse('<CFCONTENT DELETEFILE="yes" FILE="/path/to/file" RESET="no" TYPE="utf-8" VARIABLE="cfcontent_var2" />');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('content');
+        r.attributes.delete_file.should.eql(true);
+        r.attributes.file.should.eql('/path/to/file');
+        r.attributes.reset.should.eql(false);
+        r.attributes.type.should.eql('utf-8');
+        r.attributes.variable.should.eql('cfcontent_var2');
+    });
+});
