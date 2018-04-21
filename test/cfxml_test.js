53e94e8932ee6192e85e548138c00192ec8ac84f
Scott Noel-Hemming <frogstarr78@gmail.com>
2018-04-21 15:48:17 -0700

    Updated test to use Should and Mocha test frameworks

--- test/cfxml_test.js
+++ test/cfxml_test.js
@@ -1,25 +1,28 @@
-const is = require('assert'), test = require('./testlib');
+const test = require('./testlib'),
+    should = require('should');
 
-var r;
+describe("Parsing a cfxml tag", function() {
+    it('should throw an error when missing a required variable attribute', function () {
+        (function () { r = test.cfparser.parse('<cfxml case_sensitive="no">'); }).should.throw('Missing required "variable" attribute.');
+    })
 
-is.throws(function () {
-	r = test.cfparser.parse('<cfxml case_sensitive="no">');
-}, Error, "Missing required variable attribute.");
+    it('works as expected', function () {
+        r = test.cfparser.parse("<cfxml variable='cfxml_test2' />");
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('xml');
+        r.attributes.variable.should.eql('cfxml_test2');
+        r.attributes.case_sensitive.should.be.false;
 
-r = test.cfparser.parse("<cfxml variable='cfxml_test2' />");
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'xml');
-is.equal(r.attributes.variable, 'cfxml_test2');
-is.equal(r.attributes.case_sensitive, false);
+        r = test.cfparser.parse("<cfxml variable='cfxml_test3' case_sensitive='yes'>");
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('xml');
+        r.attributes.variable.should.eql('cfxml_test3');
+        r.attributes.case_sensitive.should.be.true;
 
-r = test.cfparser.parse("<cfxml variable='cfxml_test3' case_sensitive='yes'>");
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'xml');
-is.equal(r.attributes.variable, 'cfxml_test3');
-is.equal(r.attributes.case_sensitive, true);
-
-r = test.cfparser.parse("<CFXML VARIABLE='cfxml_test4' CASESENSITIVE='yes' />");
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'xml');
-is.equal(r.attributes.variable, 'cfxml_test4');
-is.equal(r.attributes.case_sensitive, true);
+        r = test.cfparser.parse("<CFXML VARIABLE='cfxml_test4' CASESENSITIVE='yes' />");
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('xml');
+        r.attributes.variable.should.eql('cfxml_test4');
+        r.attributes.case_sensitive.should.be.true;
+    })
+})
