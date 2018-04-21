31dd45a3411b2a6c9d92f1695921a0dc455bc07b
Scott Noel-Hemming <frogstarr78@gmail.com>
2018-04-21 14:25:06 -0700

    Converted to use Mocha and Should testing frameworks.

--- test/cfmailparam_test.js
+++ test/cfmailparam_test.js
@@ -1,59 +1,61 @@
-const is = require('assert'), test = require('./testlib');
+const test = require('./testlib'),
+    should = require('should');
 
-var r;
+describe("Parsing a cfmailpart tag", function() {
+    it('throws an error when missing a required action attribute', function () {
+        (function () { r = test.cfparser.parse('<cfmailparam>'); }).should.throw('Expected " ", "\\n", or "\\t" but ">" found.');
+    })
 
+    it('throws an error when missing a required action attribute', function () {
+        (function () { r = test.cfparser.parse('<cfmailparam name="cfmail_name" file="/path/to/file" />'); }).should.throw("Unexpectedly defined name and file attributes.");
+    })
 
-is.throws(function () {
-	r = test.cfparser.parse('<cfmailparam>');
-}, Error, "Missing required attributes.");
+    it('works as expected', function () {
+        r = test.cfparser.parse('<cfmailparam name="cfmailparam_test">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('mailparam');
+        r.attributes.name.should.eql('cfmailparam_test');
+        r.attributes.disposition.should.eql('attachment');
+        r.attributes.remove.should.be.false;
 
-is.throws(function () {
-	r = test.cfparser.parse('<cfmailparam name="cfmail_name" file="/path/to/file" />');
-}, Error, "Unexpectedly defined name and file attributes.");
+        r = test.cfparser.parse('<cfmailparam file="/path/to/file">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('mailparam');
+        r.attributes.file.should.eql('/path/to/file');
+        r.attributes.disposition.should.eql('attachment');
+        r.attributes.remove.should.be.false;
 
-r = test.cfparser.parse('<cfmailparam name="cfmailparam_test">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'mailparam');
-is.equal(r.attributes.name, 'cfmailparam_test');
-is.equal(r.attributes.disposition, 'attachment');
-is.equal(r.attributes.remove, false);
+        r = test.cfparser.parse('<cfmailparam name="cfmailparam_test2" disposition="inline" remove="yes" content="hello world" content_id="123abc" type="plain" value="val" />');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('mailparam');
+        r.attributes.content_id.should.eql("123abc");
+        r.attributes.disposition.should.eql("inline");
+        r.attributes.name.should.eql("cfmailparam_test2");
+        r.attributes.content.should.eql("hello world");
+        r.attributes.remove.should.be.true;
+        r.attributes.type.should.eql("plain");
+        r.attributes.value.should.eql("val");
 
-r = test.cfparser.parse('<cfmailparam file="/path/to/file">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'mailparam');
-is.equal(r.attributes.file, '/path/to/file');
-is.equal(r.attributes.disposition, 'attachment');
-is.equal(r.attributes.remove, false);
+        r = test.cfparser.parse('<cfmailparam file="/tmp/path" disposition="inline" remove="yes" content="hello world" content_id="123abc" type="text" value="val" />');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('mailparam');
+        r.attributes.content_id.should.eql("123abc");
+        r.attributes.disposition.should.eql("inline");
+        r.attributes.file.should.eql("/tmp/path");
+        r.attributes.content.should.eql("hello world");
+        r.attributes.remove.should.be.true;
+        r.attributes.type.should.eql("text");
+        r.attributes.value.should.eql("val");
 
-r = test.cfparser.parse('<cfmailparam name="cfmailparam_test2" disposition="inline" remove="yes" content="hello world" content_id="123abc" type="plain" value="val" />');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'mailparam');
-is.equal(r.attributes.content_id, "123abc");
-is.equal(r.attributes.disposition, "inline");
-is.equal(r.attributes.name, "cfmailparam_test2");
-is.equal(r.attributes.content, "hello world");
-is.equal(r.attributes.remove, true);
-is.equal(r.attributes.type, "plain");
-is.equal(r.attributes.value, "val");
-
-r = test.cfparser.parse('<cfmailparam file="/tmp/path" disposition="inline" remove="yes" content="hello world" content_id="123abc" type="text" value="val" />');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'mailparam');
-is.equal(r.attributes.content_id, "123abc");
-is.equal(r.attributes.disposition, "inline");
-is.equal(r.attributes.file, "/tmp/path");
-is.equal(r.attributes.content, "hello world");
-is.equal(r.attributes.remove, true);
-is.equal(r.attributes.type, "text");
-is.equal(r.attributes.value, "val");
-
-r = test.cfparser.parse('<CFMAILPARAM FILE="/tmp/path" DISPOSITION="inline" REMOVE="yes" CONTENT="hello world" CONTENT_ID="123abc" TYPE="html" VALUE="val" />');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'mailparam');
-is.equal(r.attributes.content_id, "123abc");
-is.equal(r.attributes.disposition, "inline");
-is.equal(r.attributes.file, "/tmp/path");
-is.equal(r.attributes.content, "hello world");
-is.equal(r.attributes.remove, true);
-is.equal(r.attributes.type, "html");
-is.equal(r.attributes.value, "val");
+        r = test.cfparser.parse('<CFMAILPARAM FILE="/tmp/path" DISPOSITION="inline" REMOVE="yes" CONTENT="hello world" CONTENT_ID="123abc" TYPE="html" VALUE="val" />');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('mailparam');
+        r.attributes.content_id.should.eql("123abc");
+        r.attributes.disposition.should.eql("inline");
+        r.attributes.file.should.eql("/tmp/path");
+        r.attributes.content.should.eql("hello world");
+        r.attributes.remove.should.be.true;
+        r.attributes.type.should.eql("html");
+        r.attributes.value.should.eql("val");
+    });
+});
