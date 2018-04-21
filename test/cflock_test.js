846590e366a816cf4e74b39645bd54b73e95592f
Scott Noel-Hemming <frogstarr78@gmail.com>
2018-04-21 15:02:58 -0700

    Updated to use Mocha and Should test frameworks

--- test/cflock_test.js
+++ test/cflock_test.js
@@ -1,41 +1,43 @@
-const is = require('assert'), test = require('./testlib');
+const should = require('should'),
+        test = require('./testlib');
 
-var r;
+describe("Parsing a cflock tag", function() {
+    it('throws an error when missing a required attributes', function () {
+        (function () { test.cfparser.parse('<cflock>'); }).should.throw('Expected " ", "\\n", or "\\t" but ">" found.');
+    })
 
-is.throws(function () {
-	r = test.cfparser.parse('<cflock>');
-}, Error, "Missing closing tag");
+    it('throws an error when missing a required timeout attribute', function () {
+        (function () { test.cfparser.parse('<cflock name="something" ></cflock>'); }).should.throw('Missing required "timeout" attribute.');
+    })
 
-is.throws(function () {
-	r = test.cfparser.parse('<cflock name="something" ></cflock>');
-}, Error, "Missing required timeout attribute");
+    it('works as expected', function () {
+        r = test.cfparser.parse('<cflock timeout="1"></cflock>');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('lock');
+        r.attributes.timeout.should.eql(1);
+        should(r.attributes.name).be.undefined;
+        r.attributes.throw_on_timeout.should.be.true;
+        r.attributes.type.should.eql('exclusive');
 
-r = test.cfparser.parse('<cflock timeout="1"></cflock>');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'lock');
-is.equal(r.attributes.timeout, 1);
-is.equal(r.attributes.name, undefined);
-is.equal(r.attributes.throw_on_timeout, true);
-is.equal(r.attributes.type, 'exclusive');
+        r = test.cfparser.parse('<cflock name="cflock" timeout="5" throw_on_timeout="no" type="readOnly">' +
+        "\n</cflock>");
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('lock');
+        r.content.should.eql("\n");
+        r.attributes.timeout.should.eql(5);
+        r.attributes.name.should.eql('cflock');
+        r.attributes.throw_on_timeout.should.be.false;
+        r.attributes.type.should.eql('readOnly');
 
-r = test.cfparser.parse('<cflock name="cflock" timeout="5" throw_on_timeout="no" type="readOnly">' +
-"\n</cflock>");
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'lock');
-is.equal(r.content, "\n");
-is.equal(r.attributes.timeout, 5);
-is.equal(r.attributes.name, 'cflock');
-is.equal(r.attributes.throw_on_timeout, false);
-is.equal(r.attributes.type, 'readOnly');
-
-r = test.cfparser.parse('<CFLOCK NAME="cflock2" TIMEOUT="6" THROW_ON_TIMEOUT="NO" TYPE="readOnly">' +
-"\nSome stuff here" + 
-"\n</CFLOCK>");
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'lock');
-is.equal(r.content, "\nSome stuff here\n");
-is.equal(r.attributes.timeout, 6);
-is.equal(r.attributes.name, 'cflock2');
-is.equal(r.attributes.throw_on_timeout, false);
-is.equal(r.attributes.type, 'readOnly');
-
+        r = test.cfparser.parse('<CFLOCK NAME="cflock2" TIMEOUT="6" THROW_ON_TIMEOUT="NO" TYPE="readOnly">' +
+        "\nSome stuff here" + 
+        "\n</CFLOCK>");
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('lock');
+        r.content.should.eql("\nSome stuff here\n");
+        r.attributes.timeout.should.eql(6);
+        r.attributes.name.should.eql('cflock2');
+        r.attributes.throw_on_timeout.should.be.false;
+        r.attributes.type.should.eql('readOnly');
+    });
+});
