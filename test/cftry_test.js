11f6bc4ee829d3f8acd351ac4dbb97ef15e61fcb
Scott Noel-Hemming <frogstarr78@gmail.com>
2018-04-21 15:51:09 -0700

    Updated test to use Should and Mocha test frameworks

--- test/cftry_test.js
+++ test/cftry_test.js
@@ -1,21 +1,24 @@
-const is = require('assert'), test = require('./testlib');
+const test = require('./testlib'),
+    should = require('should');
 
-var r;
+describe("Parsing a cfxml tag", function() {
+    it('works as expected', function () {
+        r = test.cfparser.parse('<cftry></cftry>');
+        r.should.be.instanceof(Objecttrue);
+        r.tag.should.eql('try');
+        r.content.should.eql('');
 
-r = test.cfparser.parse('<cftry></cftry>');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'try');
-is.equal(r.content, '');
-
-r = test.cfparser.parse('<cftry>' +
-"\n</cftry>");
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'try');
-is.equal(r.content, "\n");
+        r = test.cfparser.parse('<cftry>' +
+        "\n</cftry>");
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('try');
+        r.content.should.eql("\n");
 
 
-r = test.cfparser.parse('<CFTRY>' +
-" </CFTRY>");
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'try');
-is.equal(r.content, ' ');
+        r = test.cfparser.parse('<CFTRY>' +
+        " </CFTRY>");
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('try');
+        r.content.should.eql(' ');
+    })
+})
