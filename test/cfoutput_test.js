f24342973893ba6b180f56d2f8814609bcc315cb
Scott Noel-Hemming <frogstarr78@gmail.com>
2018-04-21 14:05:21 -0700

    Converted to use Mocha and Should testing frameworks.

--- test/cfoutput_test.js
+++ test/cfoutput_test.js
@@ -1,53 +1,56 @@
-const is = require('assert'), test = require('./testlib');
+const test = require('./testlib'),
+    should = require('should');
 
-var r;
+describe("Parsing a cfprocparam tag", function() {
+    it('should work as expected', function () {
+        r = test.cfparser.parse('<cfoutput ></cfoutput>');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('output');
+        r.content.should.eql('');
 
-r = test.cfparser.parse('<cfoutput ></cfoutput>');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'output');
-is.equal(r.content, '');
+        r = test.cfparser.parse('<cfoutput query="output">' +
+        "\n</cfoutput>");
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('output');
+        r.attributes.query.should.eql("output");
+        r.attributes.start_row.should.eql(1);
+        r.attributes.max_rows.should.eql(-1);
+        r.attributes.group_case_sensitive.should.be.true;
+        r.content.should.eql("\n");
 
-r = test.cfparser.parse('<cfoutput query="output">' +
-"\n</cfoutput>");
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'output');
-is.equal(r.attributes.query, "output");
-is.equal(r.attributes.start_row, 1);
-is.equal(r.attributes.max_rows, -1);
-is.equal(r.attributes.group_case_sensitive, true);
-is.equal(r.content, "\n");
+        r = test.cfparser.parse('<cfoutput query="output">' +
+        "\nThis is the content that is saved #NOW()#" +
+        "\n</cfoutput>");
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('output');
+        r.attributes.query.should.eql('output');
+        r.attributes.start_row.should.eql(1);
+        r.attributes.max_rows.should.eql(-1);
+        r.attributes.group_case_sensitive.should.be.true;
+        r.content.should.eql("\nThis is the content that is saved #NOW()#\n");
 
-r = test.cfparser.parse('<cfoutput query="output">' +
-"\nThis is the content that is saved #NOW()#" +
-"\n</cfoutput>");
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'output');
-is.equal(r.attributes.query, 'output');
-is.equal(r.attributes.start_row, 1);
-is.equal(r.attributes.max_rows, -1);
-is.equal(r.attributes.group_case_sensitive, true);
-is.equal(r.content, "\nThis is the content that is saved #NOW()#\n");
+        r = test.cfparser.parse('<cfoutput query="output" startRow="2" maxRows="10" group="id" groupCaseSensitive="no">' +
+        "\nThis is the content that is saved #NOW()#" +
+        "\n</cfoutput>");
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('output');
+        r.attributes.query.should.eql('output');
+        r.attributes.start_row.should.eql(2);
+        r.attributes.max_rows.should.eql(10);
+        r.attributes.group.should.eql('id');
+        r.attributes.group_case_sensitive.should.be.false;
+        r.content.should.eql("\nThis is the content that is saved #NOW()#\n");
 
-r = test.cfparser.parse('<cfoutput query="output" startRow="2" maxRows="10" group="id" groupCaseSensitive="no">' +
-"\nThis is the content that is saved #NOW()#" +
-"\n</cfoutput>");
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'output');
-is.equal(r.attributes.query, 'output');
-is.equal(r.attributes.start_row, 2);
-is.equal(r.attributes.max_rows, 10);
-is.equal(r.attributes.group, 'id');
-is.equal(r.attributes.group_case_sensitive, false);
-is.equal(r.content, "\nThis is the content that is saved #NOW()#\n");
-
-r = test.cfparser.parse('<CFOUTPUT QUERY="output2" STARTROW="2" MAXROWS="10" GROUP="id" GROUPCASESENSITIVE="no">' +
-"\nThis is the content that is saved #NOW()#" +
-"\n</CFOUTPUT>");
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'output');
-is.equal(r.attributes.query, 'output2');
-is.equal(r.attributes.start_row, 2);
-is.equal(r.attributes.max_rows, 10);
-is.equal(r.attributes.group, 'id');
-is.equal(r.attributes.group_case_sensitive, false);
-is.equal(r.content, "\nThis is the content that is saved #NOW()#\n");
+        r = test.cfparser.parse('<CFOUTPUT QUERY="output2" STARTROW="2" MAXROWS="10" GROUP="id" GROUPCASESENSITIVE="no">' +
+        "\nThis is the content that is saved #NOW()#" +
+        "\n</CFOUTPUT>");
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('output');
+        r.attributes.query.should.eql('output2');
+        r.attributes.start_row.should.eql(2);
+        r.attributes.max_rows.should.eql(10);
+        r.attributes.group.should.eql('id');
+        r.attributes.group_case_sensitive.should.be.false;
+        r.content.should.eql("\nThis is the content that is saved #NOW()#\n");
+    });
+});
