369d00a98da4063f3f54552e12d1df01040117be
Scott Noel-Hemming <frogstarr78@gmail.com>
2018-04-21 16:29:13 -0700

    Updated test to use Should and Mocha test frameworks

--- test/cfscript_test.js
+++ test/cfscript_test.js
@@ -1,25 +1,29 @@
-const is = require('assert'), test = require('./testlib');
+const test = require('./testlib'),
+    should = require('should');
 
-var r;
-r = test.cfparser.parse('<cfscript></cfscript>');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'script');
-is.equal(r.content, '');
+describe("Parsing a cfscript tag", function() {
+    it('woks as expected', function () {
+        r = test.cfparser.parse('<cfscript></cfscript>');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('script');
+        r.content.should.eql('');
 
-r = test.cfparser.parse('<CFSCRIPT></CFSCRIPT>');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'script');
-is.equal(r.content, '');
+        r = test.cfparser.parse('<CFSCRIPT></CFSCRIPT>');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('script');
+        r.content.should.eql('');
 
-r = test.cfparser.parse('<cfscript>something done</cfscript>');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'script');
-is.equal(r.content, 'something done');
+        r = test.cfparser.parse('<cfscript>something done</cfscript>');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('script');
+        r.content.should.eql('something done');
 
 
-r = test.cfparser.parse('<CFSCRIPT>' +
-"\nsomething more done" +
-"\n</CFSCRIPT>");
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'script');
-is.equal(r.content, "\nsomething more done\n");
+        r = test.cfparser.parse('<CFSCRIPT>' +
+        "\nsomething more done" +
+        "\n</CFSCRIPT>");
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('script');
+        r.content.should.eql("\nsomething more done\n");
+    });
+});
