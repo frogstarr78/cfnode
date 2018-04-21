6c3410902673a5a154e821017df03097230ed15b
Scott Noel-Hemming <frogstarr78@gmail.com>
2018-04-21 14:19:08 -0700

    Converted to use Mocha and Should testing frameworks.

--- test/cfmailpart_test.js
+++ test/cfmailpart_test.js
@@ -1,40 +1,42 @@
-const is = require('assert'), test = require('./testlib');
+const test = require('./testlib'),
+    should = require('should');
 
-var r;
+describe("Parsing a cfmailpart tag", function() {
+    it('throws an error when missing a required action attribute', function () {
+        (function () { r = test.cfparser.parse('<cfmailpart></cfmailpart>'); }).should.throw('Expected " ", "\\n", or "\\t" but ">" found.');
+    })
 
+    it('works as expected', function () {
+        r = test.cfparser.parse('<cfmailpart type="text"></cfmailpart>');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('mailpart');
+        r.attributes.type.should.eql('text');
+        r.attributes.wrap_text.should.eql(80);
 
-is.throws(function () {
-	r = test.cfparser.parse('<cfmailpart></cfmailpart>');
-}, Error, "Missing required attributes.");
+        r = test.cfparser.parse('<cfmailpart ' +
+        'type="text" ' + 
+        'charset="us-ascii" ' +
+        'wraptext="30">' +
+        "\nStuff" +
+        '</cfmailpart>');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('mailpart');
+        r.content.should.eql("\nStuff");
+        r.attributes.type.should.eql('text');
+        r.attributes.charset.should.eql('us-ascii');
+        r.attributes.wrap_text.should.eql(30);
 
-r = test.cfparser.parse('<cfmailpart type="text"></cfmailpart>');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'mailpart');
-is.equal(r.attributes.type, 'text');
-is.equal(r.attributes.wrap_text, 80);
-
-r = test.cfparser.parse('<cfmailpart ' +
-'type="text" ' + 
-'charset="us-ascii" ' +
-'wraptext="30">' +
-"\nStuff" +
-'</cfmailpart>');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'mailpart');
-is.equal(r.content, "\nStuff");
-is.equal(r.attributes.type, 'text');
-is.equal(r.attributes.charset, 'us-ascii');
-is.equal(r.attributes.wrap_text, 30);
-
-r = test.cfparser.parse('<CFMAILPART ' +
-'TYPE="text" ' + 
-'CHARSET="us-ascii" ' +
-'WRAPTEXT="30">' +
-"\nStuff" +
-'</CFMAILPART>');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'mailpart');
-is.equal(r.content, "\nStuff");
-is.equal(r.attributes.type, 'text');
-is.equal(r.attributes.charset, 'us-ascii');
-is.equal(r.attributes.wrap_text, 30);
+        r = test.cfparser.parse('<CFMAILPART ' +
+        'TYPE="text" ' + 
+        'CHARSET="us-ascii" ' +
+        'WRAPTEXT="30">' +
+        "\nStuff" +
+        '</CFMAILPART>');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('mailpart');
+        r.content.should.eql("\nStuff");
+        r.attributes.type.should.eql('text');
+        r.attributes.charset.should.eql('us-ascii');
+        r.attributes.wrap_text.should.eql(30);
+    });
+});
