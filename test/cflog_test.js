297e858b5d2562dde0c97e45ab1374582e55a6ff
Scott Noel-Hemming <frogstarr78@gmail.com>
2018-04-21 14:58:35 -0700

    Updated to use Mocha and Should test frameworks

--- test/cflog_test.js
+++ test/cflog_test.js
@@ -1,60 +1,61 @@
-const is = require('assert'), test = require('./testlib');
+const test = require('./testlib'),
+    should = require('should');
 
-var r;
+describe("Parsing a cflog tag", function() {
+    it('throws an error when missing a required text attribute', function () {
+        (function () { test.cfparser.parse('<cflog file="/path">'); }).should.throw('Missing required "text" attribute.');
+    })
 
+    it('works as expected', function () {
+        r = test.cfparser.parse('<cflog text="cflog test">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('log');
+        r.attributes.text.should.eql('cflog test');
+        r.attributes.application.should.be.true;
+        r.attributes.log.should.eql('application');
+        r.attributes.type.should.eql('information');
 
-is.throws(function () {
-	r = test.cfparser.parse('<cflog file="/path">');
-}, Error, "Missing required text attributes");
+        r = test.cfparser.parse('<cflog text="cflog test" file="where">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('log');
+        r.attributes.text.should.eql('cflog test');
+        r.attributes.application.should.be.true;
+        r.attributes.log.should.eql('application');
+        r.attributes.type.should.eql('information');
+        r.attributes.file.should.eql('where');
 
-r = test.cfparser.parse('<cflog text="cflog test">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'log');
-is.equal(r.attributes.text, 'cflog test');
-is.equal(r.attributes.application, true);
-is.equal(r.attributes.log,         'application');
-is.equal(r.attributes.type,        'information');
+        r = test.cfparser.parse('<cflog text="cflog test" application="true" file="log" type="fatal" log="scheduler">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('log');
+        r.attributes.text.should.eql('cflog test');
+        r.attributes.application.should.be.true;
+        r.attributes.file.should.eql('log');
+        r.attributes.log.should.eql('application');
+        r.attributes.type.should.eql('fatal');
 
-r = test.cfparser.parse('<cflog text="cflog test" file="where">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'log');
-is.equal(r.attributes.text, 'cflog test');
-is.equal(r.attributes.application, true);
-is.equal(r.attributes.log,         'application');
-is.equal(r.attributes.type,        'information');
-is.equal(r.attributes.file,        'where');
+        r = test.cfparser.parse('<CFLOG TEXT="cflog test" APPLICATION="true" FILE="log" TYPE="fatal" LOG="scheduler">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('log');
+        r.attributes.text.should.eql('cflog test');
+        r.attributes.application.should.be.true;
+        r.attributes.file.should.eql('log');
+        r.attributes.log.should.eql('application');
+        r.attributes.type.should.eql('fatal');
 
-r = test.cfparser.parse('<cflog text="cflog test" application="true" file="log" type="fatal" log="scheduler">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'log');
-is.equal(r.attributes.text, 'cflog test');
-is.equal(r.attributes.application, true);
-is.equal(r.attributes.file, 'log');
-is.equal(r.attributes.log,  'application');
-is.equal(r.attributes.type, 'fatal');
+        r = test.cfparser.parse('<cflog text="cflog test" application="true" type="fatal" log="scheduler">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('log');
+        r.attributes.text.should.eql('cflog test');
+        r.attributes.application.should.be.true;
+        should(r.attributes.file).be.undefined;
+        r.attributes.log.should.eql('scheduler');
+        r.attributes.type.should.eql('fatal');
 
-r = test.cfparser.parse('<CFLOG TEXT="cflog test" APPLICATION="true" FILE="log" TYPE="fatal" LOG="scheduler">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'log');
-is.equal(r.attributes.text, 'cflog test');
-is.equal(r.attributes.application, true);
-is.equal(r.attributes.file, 'log');
-is.equal(r.attributes.log,  'application');
-is.equal(r.attributes.type, 'fatal');
-
-r = test.cfparser.parse('<cflog text="cflog test" application="true" type="fatal" log="scheduler">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'log');
-is.equal(r.attributes.text, 'cflog test');
-is.equal(r.attributes.application, true);
-is.equal(r.attributes.file, undefined);
-is.equal(r.attributes.log,  'scheduler');
-is.equal(r.attributes.type, 'fatal');
-
-r = test.cfparser.parse('<CFLOG TEXT="cflog test" APPLICATION="true" TYPE="fatal" LOG="Scheduler">');
-is.equal(r instanceof Object, true);
-is.equal(r.attributes.application, true);
-is.equal(r.attributes.file, undefined);
-is.equal(r.attributes.log,  'scheduler');
-is.equal(r.attributes.type, 'fatal');
-
+        r = test.cfparser.parse('<CFLOG TEXT="cflog test" APPLICATION="true" TYPE="fatal" LOG="Scheduler">');
+        r.should.be.instanceof(Object);
+        r.attributes.application.should.be.true;
+        should(r.attributes.file).be.undefined;
+        r.attributes.log.should.eql('scheduler');
+        r.attributes.type.should.eql('fatal');
+    });
+});
