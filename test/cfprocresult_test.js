f49598a0e32a684bc1fc9744ef8edbdc8aaa1d8f
Scott Noel-Hemming <frogstarr78@gmail.com>
2018-04-21 13:51:44 -0700

    Converted to use Mocha and Should testing frameworks.

--- test/cfprocresult_test.js
+++ test/cfprocresult_test.js
@@ -1,27 +1,31 @@
-const is = require('assert'), test = require('./testlib');
+const test = require('./testlib'),
+    should = require('should');
 
-var r;
-is.throws(function () {
-	r = test.cfparser.parse('<cfprocresult max_rows="2">');
-}, Error, "Missing required name attribute");
+describe("Parsing a cfprocresult tag ", function() {
+    it('throws an error when missing required name attribute', function () {
+        (function () { test.cfparser.parse('<cfprocresult max_rows="2">'); }).should.throw('Missing required "name" attribute.');
+    });
 
-r = test.cfparser.parse('<cfprocresult name="procresult">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'procresult');
-is.equal(r.attributes.name, 'procresult');
-is.equal(r.attributes.max_rows, -1);
-is.equal(r.attributes.result_set, 1);
+    it('works as expected', function () {
+        r = test.cfparser.parse('<cfprocresult name="procresult">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('procresult');
+        r.attributes.name.should.eql('procresult');
+        r.attributes.max_rows.should.eql(-1);
+        r.attributes.result_set.should.eql(1);
 
-r = test.cfparser.parse('<cfprocresult maxRows="5" name="procresult2" resultSet="11">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'procresult');
-is.equal(r.attributes.name, 'procresult2');
-is.equal(r.attributes.max_rows, 5);
-is.equal(r.attributes.result_set, 11);
+        r = test.cfparser.parse('<cfprocresult maxRows="5" name="procresult2" resultSet="11">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('procresult');
+        r.attributes.name.should.eql('procresult2');
+        r.attributes.max_rows.should.eql(5);
+        r.attributes.result_set.should.eql(11);
 
-r = test.cfparser.parse('<CFPROCRESULT NAME="procresult3" MAXROWS="10" RESULTSET="11" />');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'procresult');
-is.equal(r.attributes.name, 'procresult3');
-is.equal(r.attributes.max_rows, 10);
-is.equal(r.attributes.result_set, 11);
+        r = test.cfparser.parse('<CFPROCRESULT NAME="procresult3" MAXROWS="10" RESULTSET="11" />');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('procresult');
+        r.attributes.name.should.eql('procresult3');
+        r.attributes.max_rows.should.eql(10);
+        r.attributes.result_set.should.eql(11);
+    });
+});
