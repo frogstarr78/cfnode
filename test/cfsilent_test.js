2b9a82b6318a22ba09ffcf459a603acc7a1949e3
Scott Noel-Hemming <frogstarr78@gmail.com>
2018-04-21 16:26:08 -0700

    Updated test to use Should and Mocha test frameworks

--- test/cfsilent_test.js
+++ test/cfsilent_test.js
@@ -1,21 +1,24 @@
-const is = require('assert'), test = require('./testlib');
+const test = require('./testlib'),
+    should = require('should');
 
-var r;
+describe("Parsing a cfsilent tag", function() {
+    it('woks as expected', function () {
+        r = test.cfparser.parse('<cfsilent></cfsilent>');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('silent');
+        r.content.should.eql('');
 
-r = test.cfparser.parse('<cfsilent></cfsilent>');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'silent');
-is.equal(r.content, '');
-
-r = test.cfparser.parse('<cfsilent>' +
-"\n</cfsilent>");
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'silent');
-is.equal(r.content, "\n");
+        r = test.cfparser.parse('<cfsilent>' +
+        "\n</cfsilent>");
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('silent');
+        r.content.should.eql("\n");
 
 
-r = test.cfparser.parse('<CFSILENT>' +
-" </CFSILENT>");
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'silent');
-is.equal(r.content, ' ');
+        r = test.cfparser.parse('<CFSILENT>' +
+        " </CFSILENT>");
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('silent');
+        r.content.should.eql(' ');
+    })
+})
