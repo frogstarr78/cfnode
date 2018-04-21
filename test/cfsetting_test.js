3bad9bfd3ab36b685a08b59e7e2807c63b14fa4f
Scott Noel-Hemming <frogstarr78@gmail.com>
2018-04-21 16:27:57 -0700

    Updated test to use Should and Mocha test frameworks

--- test/cfsetting_test.js
+++ test/cfsetting_test.js
@@ -1,39 +1,42 @@
-const is = require('assert'), test = require('./testlib');
+const test = require('./testlib'),
+    should = require('should');
 
-var r;
+describe("Parsing a cfsetting tag", function() {
+    it('woks as expected', function () {
+        r = test.cfparser.parse('<cfsetting>');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('setting');
 
-r = test.cfparser.parse('<cfsetting>');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'setting');
+        r = test.cfparser.parse('<cfsetting enableCFoutputOnly="0">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('setting');
+        r.attributes.enable_cfoutput_only.should.be.false;
 
-r = test.cfparser.parse('<cfsetting enableCFoutputOnly="0">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'setting');
-is.equal(r.attributes.enable_cfoutput_only, false);
+        r = test.cfparser.parse('<cfsetting requestTimeOut="4">');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('setting');
+        r.attributes.request_timeout.should.eql(4);
 
-r = test.cfparser.parse('<cfsetting requestTimeOut="4">');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'setting');
-is.equal(r.attributes.request_timeout, 4);
+        r = test.cfparser.parse('<cfsetting' +
+            ' showDebugOutput="1"' +
+            ' requestTimeOut="1"' +
+            ' enableCFoutputOnly="0"' +
+        '>');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('setting');
+        r.attributes.request_timeout.should.eql(1);
+        r.attributes.show_debug_output.should.be.true;
+        r.attributes.enable_cfoutput_only.should.be.false;
 
-r = test.cfparser.parse('<cfsetting' +
-	' showDebugOutput="1"' +
-	' requestTimeOut="1"' +
-	' enableCFoutputOnly="0"' +
-'>');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'setting');
-is.equal(r.attributes.request_timeout, 1);
-is(r.attributes.show_debug_output);
-is(!r.attributes.enable_cfoutput_only);
-
-r = test.cfparser.parse('<CFSETTING' +
-	' SHOWDEBUGOUTPUT="1"' +
-	' REQUESTTIMEOUT="1"' +
-	' ENABLECFOUTPUTONLY="0"' +
-'>');
-is.equal(r instanceof Object, true);
-is.equal(r.tag, 'setting');
-is.equal(r.attributes.request_timeout, 1);
-is(r.attributes.show_debug_output);
-is(!r.attributes.enable_cfoutput_only);
+        r = test.cfparser.parse('<CFSETTING' +
+            ' SHOWDEBUGOUTPUT="1"' +
+            ' REQUESTTIMEOUT="1"' +
+            ' ENABLECFOUTPUTONLY="0"' +
+        '>');
+        r.should.be.instanceof(Object);
+        r.tag.should.eql('setting');
+        r.attributes.request_timeout.should.eql(1);
+        r.attributes.show_debug_output.should.be.true;
+        r.attributes.enable_cfoutput_only.should.be.false;
+    });
+});
